2ef7b1188e51ff9c1cd929f13015ec982889a48d
Vitória Mendes Rodrigues <vm40124@gmail.com>
2025-10-20 14:12:35 -0300

    Create ContaBancaria class with basic methods

---
+++ exercicio3/ContaBancaria.java
@@ -0,0 +1,25 @@
+public class ContaBancaria {
+    protected String titular;
+    protected double saldo;
+
+    public ContaBancaria(String titular, double saldoInicial) {
+        this.titular = titular;
+        this.saldo = saldoInicial;
+    }
+
+    public void depositar(double valor) {
+        saldo += valor;
+    }
+
+    public void sacar(double valor) {
+        if (valor <= saldo) {
+            saldo -= valor;
+        } else {
+            System.out.println("Saldo insuficiente!");
+        }
+    }
+
+    public void exibirSaldo() {
+        System.out.println("Titular: " + titular + " | Saldo: R$ " + saldo);
+    }
+}
