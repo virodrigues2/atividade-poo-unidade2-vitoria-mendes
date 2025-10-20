bcd91176ce9577971000e65890a70090344286a9
Vitória Mendes Rodrigues <vm40124@gmail.com>
2025-10-20 15:04:14 -0300

    Create Pessoa class with name and age attributes

---
+++ exercicio5/Pessoa.java
@@ -0,0 +1,13 @@
+public class Pessoa {
+    protected String nome;
+    protected int idade;
+
+    public Pessoa(String nome, int idade) {
+        this.nome = nome;
+        this.idade = idade;
+    }
+
+    public void apresentar() {
+        System.out.println("Nome: " + nome + " | Idade: " + idade);
+    }
+}
