a05de5a083623675c3c72641bcf58973d0b8df18
Vitória Mendes Rodrigues <vm40124@gmail.com>
2025-10-20 15:14:15 -0300

    Create MainEmpresa.java

---
+++ exercicio8/MainEmpresa.java
@@ -0,0 +1,7 @@
+public class MainEmpresa {
+    public static void main(String[] args) {
+        Funcionario f = new Funcionario("Vitória");
+        f.trabalhar();
+        f.descansar();
+    }
+}
