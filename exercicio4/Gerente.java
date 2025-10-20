2aa331cc62c90570bb9ec27658c2749709621a30
Vitória Mendes Rodrigues <vm40124@gmail.com>
2025-10-20 15:01:12 -0300

    Add Gerente class extending Funcionario

---
+++ exercicio4/Gerente.java
@@ -0,0 +1,13 @@
+public class Gerente extends Funcionario {
+    private double bonusAdicional;
+
+    public Gerente(String nome, double salario, double bonusAdicional) {
+        super(nome, salario);
+        this.bonusAdicional = bonusAdicional;
+    }
+
+    @Override
+    public double calcularBonus() {
+        return super.calcularBonus() + bonusAdicional;
+    }
+}
