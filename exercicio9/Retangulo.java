8d66853b48ffbe2d02bc4000bf9ae4ee2807afaf
Vitória Mendes Rodrigues <vm40124@gmail.com>
2025-10-20 15:17:05 -0300

    Create Retangulo.java

---
+++ exercicio9/Retangulo.java
@@ -0,0 +1,13 @@
+public class Retangulo extends Forma {
+    private double largura;
+    private double altura;
+
+    public Retangulo(double largura, double altura) {
+        this.largura = largura;
+        this.altura = altura;
+    }
+
+    public double calcularArea() {
+        return largura * altura;
+    }
+}
