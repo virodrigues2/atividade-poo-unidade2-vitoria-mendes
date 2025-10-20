cafc5763ef4191ae4f3b0d934f2c65fb435af864
Vitória Mendes Rodrigues <vm40124@gmail.com>
2025-10-20 15:04:48 -0300

    Add Aluno class extending Pessoa

---
+++ exercicio5/Aluno.java
@@ -0,0 +1,13 @@
+public class Aluno extends Pessoa {
+    private String curso;
+
+    public Aluno(String nome, int idade, String curso) {
+        super(nome, idade);
+        this.curso = curso;
+    }
+
+    @Override
+    public void apresentar() {
+        System.out.println("Aluno: " + nome + " | Curso: " + curso);
+    }
+}
